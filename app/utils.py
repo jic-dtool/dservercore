d6d09d6d6420dc147d73f94595767b4d8ce5c66d
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-03-01 11:39:21 +0000

    Add more logic to dataset_info_is_valid function and restructure tests

--- app/utils.py
+++ app/utils.py
@@ -2,4 +2,10 @@
 
 def dataset_info_is_valid(dataset_info):
     """Return True if the dataset info is valid."""
+    if "uuid" not in dataset_info:
+        return False
+    if "type" not in dataset_info:
+        return False
+    if "uri" not in dataset_info:
+        return False
     return True
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ _HERE = os.path.dirname(os.path.abspath(__file__))
 _ROOT = os.path.join(_HERE, "..")
 sys.path.insert(0, _ROOT)
 
-def test_dataset_info_is_valid():
+def test_dataset_info_is_valid_returns_true_on_valid_info():
     from app.utils import dataset_info_is_valid
 
     # Below is the minimum data required to register a dataset.
@@ -15,3 +15,25 @@ def test_dataset_info_is_valid():
       "uri": "file:///tmp/a_dataset"
     }
     assert dataset_info_is_valid(info)
+
+
+def test_dataset_info_returns_false_when_key_data_is_missing():
+    from app.utils import dataset_info_is_valid
+
+    info = {
+      "type": "dataset",
+      "uri": "file:///tmp/a_dataset"
+    }
+    assert not dataset_info_is_valid(info)
+
+    info = {
+      "uuid": "af6727bf-29c7-43dd-b42f-a5d7ede28337",
+      "uri": "file:///tmp/a_dataset"
+    }
+    assert not dataset_info_is_valid(info)
+
+    info = {
+      "uuid": "af6727bf-29c7-43dd-b42f-a5d7ede28337",
+      "type": "dataset",
+    }
+    assert not dataset_info_is_valid(info)
