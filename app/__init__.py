3947bcd6ab70c64a1fcb5d5742e238775fcb5180
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-03-01 18:01:43 +0000

    Add validation of dataset info before adding it to mongodb

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+
+from utils import dataset_info_is_valid
 
 app = Flask(__name__)
 
@@ -31,5 +33,7 @@ def lookup_dataset_info(uuid):
 @app.route("/register_dataset_info", methods=["POST"])
 def register_dataset_info():
     dataset_info = request.get_json()
+    if not dataset_info_is_valid(dataset_info):
+        abort(400)
     i = app.config["mongo_collection"].insert_one(dataset_info).inserted_id
     return str(i)
--- tests/test_register_dataset_info.py
+++ tests/test_register_dataset_info.py
@@ -12,12 +12,32 @@ def test_register_dataset(tmp_app):  # NOQA
         "type": "dataset",
         "uri": "file:///tmp/a_dataset"
     }
-    tmp_app.post(
+    r = tmp_app.post(
         "/register_dataset_info",
         data=json.dumps(data),
         content_type='application/json'
     )
 
+    assert r.status_code == 200
+
     lookup_url = "/lookup_dataset_info/{}".format(uuid)
     r = tmp_app.get(lookup_url)
     assert data == json.loads(r.data)
+
+
+def test_register_dataset_raises_bad_request_when_dataset_info_is_invalid(tmp_app):  # NOQA
+    uuid = "af6727bf-29c7-43dd-b42f-a5d7ede28337"
+    data = {
+        "uuid": uuid,
+        "type": "protodataset",
+        "uri": "file:///tmp/a_dataset"
+    }
+
+    r = tmp_app.post(
+        "/register_dataset_info",
+        data=json.dumps(data),
+        content_type='application/json'
+    )
+
+    assert r.status_code == 400
+
