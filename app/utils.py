375dfa96e993c048df74ac9ca88743e18186afd5
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-03-01 11:48:37 +0000

    Add basic uuid santiy checking

--- app/utils.py
+++ app/utils.py
@@ -10,4 +10,6 @@ def dataset_info_is_valid(dataset_info):
         return False
     if dataset_info["type"] != "dataset":
         return False
+    if len(dataset_info["uuid"]) != 36:
+        return False
     return True
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -38,11 +38,23 @@ def test_dataset_info_returns_false_when_key_data_is_missing():
     }
     assert not dataset_info_is_valid(info)
 
+
 def test_dataset_info_returns_false_when_type_is_not_dataset():
     from app.utils import dataset_info_is_valid
 
     info = {
       "uuid": "af6727bf-29c7-43dd-b42f-a5d7ede28337",
+      "type": "protodataset",
+      "uri": "file:///tmp/a_dataset"
+    }
+    assert not dataset_info_is_valid(info)
+
+
+def test_dataset_info_returns_false_if_uuid_looks_invalid():
+    from app.utils import dataset_info_is_valid
+
+    info = {
+      "uuid": "af6727bf-29c7-43dd-b42f",
       "type": "protodataset",
       "uri": "file:///tmp/a_dataset"
     }
