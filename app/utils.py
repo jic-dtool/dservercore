47fe96cd6e388c446a63786e61560bac1ec02298
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-03-02 11:11:40 +0000

    Add utitlity functions for interacting with mongodb

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,7 @@
 """Utility functions."""
 
+import pymongo
+
 def dataset_info_is_valid(dataset_info):
     """Return True if the dataset info is valid."""
     if "uuid" not in dataset_info:
@@ -13,3 +15,40 @@ def dataset_info_is_valid(dataset_info):
     if len(dataset_info["uuid"]) != 36:
         return False
     return True
+
+
+def num_datasets(collection):
+    """Return the number of datasets in the mongodb collection."""
+    return collection.count()
+
+
+def register_dataset(collection, dataset_info):
+    """Register dataset info in the collection."""
+    if not dataset_info_is_valid(dataset_info):
+        return None
+
+    # If a record with the same UUID and URI exists return the uuid
+    # without adding a duplicate record.
+    exists = collection.find_one({
+        "uuid": dataset_info["uuid"],
+        "uri": dataset_info["uri"]
+    })
+
+    if exists is None:
+        collection.insert_one(dataset_info)
+
+    # The MongoDB client dynamically updates the dataset_info dict
+    # with and '_id' key. Remove it.
+    if "_id" in dataset_info:
+        del dataset_info["_id"]
+
+    return dataset_info["uuid"]
+
+
+def lookup_datasets(collection, uuid):
+    """Return list of dataset info dictionaries with matching uuid."""
+    result = []
+    for info in collection.find({"uuid": uuid}):
+        del info["_id"]
+        result.append(info)
+    return result
---
+++ tests/test_utils_mongodb_helpers.py
@@ -0,0 +1,76 @@
+"""Test mongodb utility helper functions."""
+
+from pymongo import MongoClient
+
+def test_mongodb_utiliites_functional():
+
+    from app.utils import (
+        num_datasets,
+        register_dataset,
+        lookup_datasets,
+    )
+
+    db_name = "testing_db"
+    client = MongoClient()
+    db = client[db_name]
+    try:
+
+        # At the start the collection is empty.
+        collection = db["datasets"]
+        assert num_datasets(collection) == 0
+
+        # It is possible to register dataset in the collection by supplying
+        # the relevant information.
+        info = {
+          u"uuid": u"af6727bf-29c7-43dd-b42f-a5d7ede28337",
+          u"type": u"dataset",
+          u"uri": u"file:///tmp/a_dataset"
+        }
+        uuid = register_dataset(collection, info)
+        assert "_id" not in info
+        assert uuid == info["uuid"]
+        assert num_datasets(collection) == 1
+
+        # Trying to register the same dataset information twice results
+        # in the function returning the uuid, but without adding another
+        # entry to the mongo database.
+        uuid = register_dataset(collection, info)
+        assert uuid == info["uuid"]
+        assert num_datasets(collection) == 1
+
+        # Trying to register a dataset using invalid information results
+        # in the function returning None.
+        assert register_dataset(collection, {"not": "valid"}) is None
+
+        # Register a second dataset.
+        info_2 = {
+          u"uuid": u"c58038a4-3a54-425e-9087-144d0733387f",
+          u"type": u"dataset",
+          u"uri": u"file:///tmp/another_dataset"
+        }
+        uuid_2 = register_dataset(collection, info_2)
+        assert num_datasets(collection) == 2
+
+        # The same dataset can be registered in more than one location.
+        # Register a second dataset.
+        info_2_alt = {
+          u"uuid": u"c58038a4-3a54-425e-9087-144d0733387f",
+          u"type": u"dataset",
+          u"uri": u"s3:/test-dtool-s3-bucket/c58038a4-3a54-425e-9087-144d0733387f"
+        }
+        assert register_dataset(collection, info_2_alt) == uuid_2
+        assert num_datasets(collection) == 3
+
+        # One can lookup dataset information using the UUID. This returns
+        # a list with all the matching entries.
+        lookup_result = lookup_datasets(collection, uuid)
+        assert len(lookup_result) == 1
+        assert "_id" not in lookup_result[0]
+        assert lookup_result == [info]
+
+        lookup_result = lookup_datasets(collection, uuid_2)
+        assert len(lookup_result) == 2
+        assert sorted(lookup_result) == sorted([info_2, info_2_alt])
+
+    finally:
+        client.drop_database(db_name)
