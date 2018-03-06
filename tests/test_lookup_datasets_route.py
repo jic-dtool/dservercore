8f8cb6d91a7a9dd34def6371a31c5e9001f66374
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-03-06 10:21:57 +0000

    Add test for lookup_datasets route

---
+++ tests/test_lookup_datasets_route.py
@@ -0,0 +1,26 @@
+"""Test the /lookup_datasets/<uuid> route."""
+
+import json
+
+from . import tmp_app  # NOQA
+
+
+def test_lookup_datasets_route(tmp_app):  # NOQA
+
+    # Register a dataset to look up.
+    from app.utils import register_dataset
+
+    uuid = "af6727bf-29c7-43dd-b42f-a5d7ede28337"
+    data = {
+        "uuid": uuid,
+        "type": "dataset",
+        "uri": "file:///tmp/a_dataset"
+    }
+
+    collection = tmp_app.application.config["mongo_collection"]
+    register_dataset(collection, data)
+
+
+    lookup_url = "/lookup_datasets/{}".format(uuid)
+    r = tmp_app.get(lookup_url)
+    assert [data] == json.loads(r.data)
