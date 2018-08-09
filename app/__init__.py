4d0abfd5f229dcd5d4ecd60bd37ec1122c4a4f34
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-08-09 12:01:06 +0100

    Add entire document wild card search indexing

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, abort
+import pymongo
 from pymongo import MongoClient
 
 from . import utils
@@ -15,6 +16,9 @@ collection = db["datasets"]
 app.config["mongo_client"] = client
 app.config["mongo_db"] = db
 app.config["mongo_collection"] = collection
+
+# Index entire document.
+collection.create_index([("$**", pymongo.TEXT)])
 
 
 @app.route("/")
