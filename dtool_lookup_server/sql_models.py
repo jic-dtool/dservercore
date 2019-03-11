d85134b1aee3a4865801b8eb8bde047b3c3a3364
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-03-11 16:51:42 +0000

    Add is_admin column to user table

--- dtool_lookup_server/sql_models.py
+++ dtool_lookup_server/sql_models.py
@@ -3,6 +3,7 @@ from dtool_lookup_server import sql_db as db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    is_admin = db.Column(db.Boolean(), nullable=False, default=False)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}, is_admin={}>'.format(self.username, self.is_admin)
