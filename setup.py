a48dcc661da4091216647f616824147bac37f13c
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-03-14 14:54:44 +0000

    Standardiese use of quotes in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='dtool-lookup-server',
-    packages=['dtool_lookup_server'],
+    name="dtool-lookup-server",
+    packages=["dtool_lookup_server"],
     include_package_data=True,
     package_data={"dtool_lookup_server": ["templates/*"]},
     install_requires=[
-        'flask',
-        'flask-sqlalchemy',
-        'flask-migrate',
-        'flask-pymongo',
-        'dtoolcore',
-        'dtool_irods',
-        'dtool_s3',
-        'pyyaml',
+        "flask",
+        "flask-sqlalchemy",
+        "flask-migrate",
+        "flask-pymongo",
+        "dtoolcore",
+        "dtool_irods",
+        "dtool_s3",
+        "pyyaml",
     ],
 )
