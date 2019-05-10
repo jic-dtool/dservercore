fe210f79f196649c7e272a2f68ee3e157270cb8d
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-05-10 12:00:11 +0100

    Add --pid argument when start_gunicorn.sh script

--- provision/roles/dtool_lookup_server/files/start_gunicorn.sh
+++ provision/roles/dtool_lookup_server/files/start_gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source load_env.sh
-exec gunicorn -D -b :5000 --access-logfile /home/dtool/logs/access.log --error-logfile /home/dtool/logs/error.log "dtool_lookup_server:create_app()"
+exec gunicorn -D -b :5000 --access-logfile /home/dtool/logs/access.log --error-logfile /home/dtool/logs/error.log --pid /home/dtool/gunicorn.pid "dtool_lookup_server:create_app()"
