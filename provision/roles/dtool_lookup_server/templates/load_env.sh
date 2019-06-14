db8f955d5ca2ca89657dd5c53bda6e64546d234d
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-06-14 21:34:54 +0100

    Add ability to specify JWT_PUBLIC_KEY as a string and use in provisioning

---
+++ provision/roles/dtool_lookup_server/templates/load_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#export JWT_PRIVATE_KEY_FILE=/home/dtool/id_rsa
+#export JWT_PUBLIC_KEY_FILE=/home/dtool/id_rsa.pub
+export JWT_PUBLIC_KEY={{ JWT_PUBLIC_KEY }}
+export SQLALCHEMY_DATABASE_URI=sqlite:////home/dtool/app.db
+export FLASK_APP="dtool_lookup_server:create_app()"
