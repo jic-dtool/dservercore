54d1373b3ad2f0890c52206ba9bfb4a918a75332
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-06-20 17:41:52 +0100

    Add registering of data and command to register everything

--- registration_utils/register.py
+++ registration_utils/register.py
@@ -2,6 +2,11 @@ import click
 import requests
 import yaml
 
+from dtool_lookup_server.utils import (
+    iter_datasets_in_base_uri,
+    generate_dataset_info,
+)
+
 with open("projects.yml") as fh:
     PROJECTS = yaml.load(fh, Loader=yaml.FullLoader)
 
@@ -14,16 +19,7 @@ def get_header(token):
     }
 
 
-
-@click.group()
-def register():
-    "Register base URIs, users, permissions and data in dtool-lookup-server."
-
-
-@register.command()
-@click.argument("token")
-@click.argument("lookup_server_url")
-def base_uris(token, lookup_server_url):
+def register_base_uris(token, lookup_server_url):
     "Register base URIs."
     for b_uri in PROJECTS.keys():
         data = {"base_uri": b_uri}
@@ -36,10 +32,7 @@ def base_uris(token, lookup_server_url):
         print(response.status_code, response.reason)
 
 
-@register.command()
-@click.argument("token")
-@click.argument("lookup_server_url")
-def users(token, lookup_server_url):
+def register_users(token, lookup_server_url):
     "Register users."
     users = set()
     for base_uri, permissions in PROJECTS.items():
@@ -59,10 +52,7 @@ def users(token, lookup_server_url):
     print(response.status_code, response.reason)
 
 
-@register.command()
-@click.argument("token")
-@click.argument("lookup_server_url")
-def permissions(token, lookup_server_url):
+def register_permissions(token, lookup_server_url):
     "Register permissions."
     for b_uri, permissions in PROJECTS.items():
         data = {"base_uri": b_uri}
@@ -84,6 +74,77 @@ def permissions(token, lookup_server_url):
         )
         print(data)
         print(response.status_code, response.reason)
+
+
+def register_data(token, lookup_server_url):
+    "Register data."
+    for b_uri in PROJECTS.keys():
+        for dataset in iter_datasets_in_base_uri(b_uri):
+            print(dataset.uri)
+            dataset_info = generate_dataset_info(dataset, b_uri)
+            response = requests.post(
+                lookup_server_url + "/dataset/register",
+                headers=get_header(token),
+                json=dataset_info
+            )
+            print(response.status_code, response.reason)
+
+
+@click.group()
+def register():
+    "Register base URIs, users, permissions and data in dtool-lookup-server."
+
+
+@register.command()
+@click.argument("token")
+@click.argument("lookup_server_url")
+def base_uris(token, lookup_server_url):
+    "Register base URIs."
+    register_base_uris(token, lookup_server_url)
+
+
+@register.command()
+@click.argument("token")
+@click.argument("lookup_server_url")
+def users(token, lookup_server_url):
+    "Register users."
+    register_users(token, lookup_server_url)
+
+
+@register.command()
+@click.argument("token")
+@click.argument("lookup_server_url")
+def permissions(token, lookup_server_url):
+    "Register permissions."
+    register_permissions(token, lookup_server_url)
+
+
+@register.command()
+@click.argument("token")
+@click.argument("lookup_server_url")
+def data(token, lookup_server_url):
+    "Register data."
+    for b_uri in PROJECTS.keys():
+        for dataset in iter_datasets_in_base_uri(b_uri):
+            print(dataset.uri)
+            dataset_info = generate_dataset_info(dataset, b_uri)
+            response = requests.post(
+                lookup_server_url + "/dataset/register",
+                headers=get_header(token),
+                json=dataset_info
+            )
+            print(response.status_code, response.reason)
+
+
+@register.command()
+@click.argument("token")
+@click.argument("lookup_server_url")
+def all(token, lookup_server_url):
+    "Register base URI, users, permissions and data."
+    register_base_uris(token, lookup_server_url)
+    register_users(token, lookup_server_url)
+    register_permissions(token, lookup_server_url)
+    register_data(token, lookup_server_url)
 
 
 if __name__ == "__main__":
