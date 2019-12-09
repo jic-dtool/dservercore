3ba0d2f47efbb101dc923740efba7199be359901
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-12-09 16:11:28 +0000

    Add bash script for getting token from token server

---
+++ registration_utils/get_token.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Usage: ./get_token.sh <username> <token_server>
+
+USERNAME=$1
+TOKEN_SERVER=$2
+
+if [ -z "$USERNAME" ]
+then
+    echo "Please specify the username"
+    exit 1
+fi
+
+if [ -z "$TOKEN_SERVER" ]
+then
+    echo "Please specify the token server"
+    exit 1
+fi
+
+read -s -p "Password: " PASSWORD
+echo ""
+
+
+TOKEN=$(curl -H "Content-Type: application/json"  -X POST -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\" }"  $TOKEN_SERVER/token | cut -d\" -f8)
+
+if [ -z "$TOKEN" ]
+then
+    echo "Incorrect password"
+    exit 1
+fi
+
+echo $TOKEN
