e9dff77d617b1537c0dabe325572f23c26eb1310
Tjelvar Olsson <tjelvar.olsson@gmail.com>
2022-09-05 20:56:25 +0100

    Copy over tests for _dict_to_mongo_query helper function

--- tests/test_utils_search.py
+++ tests/test_utils_search.py
@@ -1,9 +1,10 @@
 """Test the search utility functions."""
 
-# TODO: copy over all the tests from test_dict_to_mongo_query.py
+# DONE: copy over all the tests from test_dict_to_mongo_query.py
 # DONE: rework and add all search tests from test_dataset_routes.py
 # TODO: add tests and functionality for mongo_search.register_dataset()
 # TODO: add tests and functionality for mongo_search.lookup_uris()
+# TODO: rework test fixture to make it stand alone from dtool-lookup-server (use register_dataset to make it easier to build it up)
 
 import pytest
 import pymongo
@@ -117,3 +118,91 @@ def test_functional(tmp_app_with_data):  # NOQA
                 )
                 result = mongo_search.search(query)
                 assert len(result) == 0
+
+
+# Test the _dict_to_mongo_query helper function.
+from dtool_lookup_server.utils_search import _dict_to_mongo_query
+
+def test_empty_dict():
+    """An empty dict should return query for all datasets."""
+    assert _dict_to_mongo_query({}) == {}
+
+
+def test_free_text():
+    """Should return {"$text": {"$search": "free_text_here"}}"""
+    query = dict(free_text="free_text_here")
+    expected_mongo_query = {"$text": {"$search": "free_text_here"}}
+    assert _dict_to_mongo_query(query) == expected_mongo_query
+
+
+def test_creator_usernames():
+    # Test single creator username.
+    query = dict(creator_usernames=["grumpy"])
+    expected_mongo_query = {"creator_username": "grumpy"}
+    assert _dict_to_mongo_query(query) == expected_mongo_query
+
+    # Test multiple creator usernames.
+    query = dict(creator_usernames=["grumpy", "dopey"])
+    expected_mongo_query = {"$or": [
+        {"creator_username": "grumpy"},
+        {"creator_username": "dopey"}
+    ]}
+    assert _dict_to_mongo_query(query) == expected_mongo_query
+
+    # Test empty list.
+    query = dict(creator_usernames=[])
+    assert _dict_to_mongo_query(query) == {}
+
+
+def test_base_uris():
+    # Test single base URI.
+    query = dict(base_uris=["s3://snow-white"])
+    expected_mongo_query = {"base_uri": "s3://snow-white"}
+    assert _dict_to_mongo_query(query) == expected_mongo_query
+
+    # Test multiple base URIs.
+    query = dict(base_uris=["s3://snow-white", "s3://mr-men"])
+    expected_mongo_query = {"$or": [
+        {"base_uri": "s3://snow-white"},
+        {"base_uri": "s3://mr-men"}
+    ]}
+    assert _dict_to_mongo_query(query) == expected_mongo_query
+
+
+def test_tags():
+    # Test single tag.
+    query = dict(tags=["evil"])
+    expected_mongo_query = {"tags": "evil"}
+    assert _dict_to_mongo_query(query) == expected_mongo_query
+
+    # Test multiple tags.
+    query = dict(tags=["evil", "good"])
+    expected_mongo_query = {"tags": {"$all": ["evil", "good"]}}
+    assert _dict_to_mongo_query(query) == expected_mongo_query
+
+    # Test empty list.
+    query = dict(tags=[])
+    assert _dict_to_mongo_query(query) == {}
+
+
+def test_combinations():
+    query = dict(
+        free_text="apple",
+        base_uris=["s3://snow-white"],
+        creator_usernames=["grumpy", "dopey"],
+        tags=["good", "evil"]
+    )
+    expected_mongo_query = {}
+    expected_mongo_query = {
+        "$and": [
+            {"$text": {"$search": "apple"}},
+            {"$or": [
+                {"creator_username": "grumpy"},
+                {"creator_username": "dopey"}
+            ]
+            },
+            {"base_uri": "s3://snow-white"},
+            {"tags": {"$all": ["good", "evil"]}}
+        ]
+    }
+    assert _dict_to_mongo_query(query) == expected_mongo_query
