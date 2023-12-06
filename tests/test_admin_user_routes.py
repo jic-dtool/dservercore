479d4405c13c9c8c733e9eeafa7acc01e1d7bb26
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2023-12-06 16:35:16 +0100

    TST: remodeled user registration test

--- tests/test_admin_user_routes.py
+++ tests/test_admin_user_routes.py
@@ -23,46 +23,53 @@ def test_register_user_route(tmp_app_with_users):  # NOQA
         {"username": "dopey"}
     ]
     headers = dict(Authorization="Bearer " + snowwhite_token)
-    r = tmp_app_with_users.post(
-        "/admin/user/register",
-        headers=headers,
-        data=json.dumps(users),
-        content_type="application/json"
-    )
-    assert r.status_code == 201
+    for user in users:
+        r = tmp_app_with_users.post(
+            "/users/{}".format(user["username"]),
+            headers=headers,
+            data=json.dumps(user),
+            content_type="application/json"
+        )
+        assert r.status_code == 201
+
     assert user_exists("evil-witch")
     assert user_exists("dopey")
 
     # Ensure idempotent.
-    r = tmp_app_with_users.post(
-        "/admin/user/register",
-        headers=headers,
-        data=json.dumps(users),
-        content_type="application/json"
-    )
-    assert r.status_code == 201
+    for user in users:
+        r = tmp_app_with_users.post(
+            "/users/{}".format(user["username"]),
+            headers=headers,
+            data=json.dumps(user),
+            content_type="application/json"
+        )
+        assert r.status_code == 201
+
     assert user_exists("evil-witch")
     assert user_exists("dopey")
 
     # Only admins allowed. However, don't give away that URL exists to
     # non-admins.
     headers = dict(Authorization="Bearer " + grumpy_token)
-    r = tmp_app_with_users.post(
-        "/admin/user/register",
-        headers=headers,
-        data=json.dumps(users),
-        content_type="application/json"
-    )
-    assert r.status_code == 404
+
+    for user in users:
+        r = tmp_app_with_users.post(
+            "/users/{}".format(user["username"]),
+            headers=headers,
+            data=json.dumps(user),
+            content_type="application/json"
+        )
+        assert r.status_code == 404
 
     headers = dict(Authorization="Bearer " + noone_token)
-    r = tmp_app_with_users.post(
-        "/admin/user/register",
-        headers=headers,
-        data=json.dumps(users),
-        content_type="application/json"
-    )
-    assert r.status_code == 404
+    for user in users:
+        r = tmp_app_with_users.post(
+            "/users/{}".format(user["username"]),
+            headers=headers,
+            data=json.dumps(user),
+            content_type="application/json"
+        )
+        assert r.status_code == 404
 
 
 def test_list_user_route(tmp_app_with_users):  # NOQA
