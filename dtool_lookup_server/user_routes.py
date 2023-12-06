ff323ac89136484feef8d47a2b7950e88beff1c5
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2023-12-06 16:34:15 +0100

    MAINT: remodeled user admin routes

--- dtool_lookup_server/user_routes.py
+++ dtool_lookup_server/user_routes.py
@@ -15,7 +15,7 @@ from dtool_lookup_server.blueprint import Blueprint
 from dtool_lookup_server.sort import SortParameters
 from dtool_lookup_server.schemas import RegisterUserSchema, UserResponseSchema
 from dtool_lookup_server.sql_models import User, UserSchema
-from dtool_lookup_server.utils import register_users
+from dtool_lookup_server.utils import register_user, update_user, delete_user
 
 
 bp = Blueprint("users", __name__, url_prefix="/users")
@@ -31,8 +31,8 @@ def list_users(pagination_parameters: PaginationParameters, sort_parameters: Sor
 
     The user in the Authorization token needs to be admin.
     """
-    username = get_jwt_identity()
-    if not dtool_lookup_server.utils_auth.has_admin_rights(username):
+    identity = get_jwt_identity()
+    if not dtool_lookup_server.utils_auth.has_admin_rights(identity):
         abort(404)
 
     query = User.query.filter_by()
@@ -58,31 +58,31 @@ def get_user_info(username):
     A user can see his/her own profile.
     An admin user can see other user's profiles.
     """
-    token_username = get_jwt_identity()
+    identity = get_jwt_identity()
 
-    if not dtool_lookup_server.utils_auth.user_exists(token_username):
+    if not dtool_lookup_server.utils_auth.user_exists(identity):
         # Unregistered users should see 404.
         abort(404)
 
     # Return 404 if the user is not admin and the token username
     # does not match up with the username in the URL.
-    if not dtool_lookup_server.utils_auth.has_admin_rights(token_username):
-        if token_username != username:
+    if not dtool_lookup_server.utils_auth.has_admin_rights(identity):
+        if identity != username:
             abort(404)
 
     return dtool_lookup_server.utils.get_user_info(username)
 
 
-@bp.route("/users/<username>", methods=["POST"])
-@bp.arguments(RegisterUserSchema(many=True, partial=("is_admin",)))
+@bp.route("/<username>", methods=["POST"])
+@bp.arguments(RegisterUserSchema(many=False, partial=("username", "is_admin",)))
 @jwt_required()
-def register(data: RegisterUserSchema):
+def register(data: RegisterUserSchema, username):
     """Register a user in the dtool lookup server.
 
     The user in the Authorization token needs to be admin.
     """
-    username = get_jwt_identity()
-    if not dtool_lookup_server.utils_auth.has_admin_rights(username):
+    identity = get_jwt_identity()
+    if not dtool_lookup_server.utils_auth.has_admin_rights(identity):
         abort(404)
 
     #   # Make it idempotent.
@@ -91,6 +91,39 @@ def register(data: RegisterUserSchema):
 
     # There should be some validation of the input here...
 
-    register_users(data)
+    register_user(username, data)
 
     return "", 201
+
+
+@bp.route("/<username>", methods=["PUT", "PATCH"])
+@bp.arguments(RegisterUserSchema(many=False, partial=("username", "is_admin",)))
+@jwt_required()
+def update(data: RegisterUserSchema, username):
+    """Update a user in the dtool lookup server.
+
+    The user in the Authorization token needs to be admin.
+    """
+    identity = get_jwt_identity()
+    if not dtool_lookup_server.utils_auth.has_admin_rights(identity):
+        abort(404)
+
+    update_user(username, data)
+
+    return "", 200
+
+
+@bp.route("/<username>", methods=["DELETE"])
+@jwt_required()
+def delete(username):
+    """Delete a user from the dtool lookup server.
+
+    The user in the Authorization token needs to be admin.
+    """
+    identity = get_jwt_identity()
+    if not dtool_lookup_server.utils_auth.has_admin_rights(identity):
+        abort(404)
+
+    delete_user(username)
+
+    return "", 200
