e361594de4844d07563f829c2c512e110d9de7e5
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2024-01-31 18:20:36 +0100

    TST: added order check to test_list_uri_route

--- tests/test_uri_routes.py
+++ tests/test_uri_routes.py
@@ -20,10 +20,43 @@ def test_list_uri_route(
     hits = json.loads(r.data.decode("utf-8"))
     assert len(hits) == 3
 
+    bad_apples_on_mr_men = {
+        'base_uri': 's3://mr-men',
+        'created_at': 1536238185.881941,
+        'creator_username': 'queen',
+        'frozen_at': 1536238185.881941,
+        'name': 'bad-apples',
+        'uri': 's3://mr-men/af6727bf-29c7-43dd-b42f-a5d7ede28337',
+        'uuid': 'af6727bf-29c7-43dd-b42f-a5d7ede28337'
+    }
+    oranges_on_snow_white = {
+        'base_uri': 's3://snow-white',
+        'created_at': 1536238185.881941,
+        'creator_username': 'queen',
+        'frozen_at': 1536238185.881941,
+        'name': 'oranges',
+        'uri': 's3://snow-white/a2218059-5bd0-4690-b090-062faf08e046',
+        'uuid': 'a2218059-5bd0-4690-b090-062faf08e046'
+    }
+    bad_apples_on_snow_white = {
+        'base_uri': 's3://snow-white',
+        'created_at': 1536238185.881941,
+        'creator_username': 'queen',
+        'frozen_at': 1536238185.881941,
+        'name': 'bad-apples',
+        'uri': 's3://snow-white/af6727bf-29c7-43dd-b42f-a5d7ede28337',
+        'uuid': 'af6727bf-29c7-43dd-b42f-a5d7ede28337'
+    }
+
     # Make sure that timestamps are returned as float.
     first_entry = hits[0]
     assert isinstance(first_entry["created_at"], float)
     assert isinstance(first_entry["frozen_at"], float)
+
+    expected_order = [
+        bad_apples_on_mr_men, oranges_on_snow_white, bad_apples_on_snow_white
+    ]
+    assert hits == expected_order
 
     r = tmp_app_with_data_client.get(
         "/uris",
